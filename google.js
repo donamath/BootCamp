4303e3533237ae05626b34efafd543eaf29a70d4
@Donamath <dona123akd@gmail.com>
2021-05-21 00:28:36 +0530

    add files

--- google.js
+++ google.js
@@ -105,4 +105,24 @@ console.log(`My age\t is ${a}.\n What\bab\\t y?`);
 
 /***********************************************************************/
 
+// Declare the myGlobal variable below this line
+var myGlobal=10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+var oopsGlobal=5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
 
